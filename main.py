7cf016ae006ee44aac6503968918976bee7de356
Leo Qiao <qiaofeitong@hotmail.com>
2018-04-13 14:04:39 -0400

    update

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ if __name__ == "__main__":
     asm_fname = sys.argv[1]
 
     # Initialization
+    psr = Parser(asm_fname)
+    sym_table = SymbolTable()
 
     # First Pass: symbol table, hack line count
+    line_count = 0
+    while psr.has_more_commands():
+        psr.advance()
+        if psr.command_type() == "L_COMMAND":
+            psr.parse_command()
+            if not sym_table.contains(psr.symbol):
+                sym_table.add_entry(psr.symbol)
 
     # Second Pass: parsing, writing to .hack file
